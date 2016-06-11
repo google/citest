6c42739e123a1c23793302b8bbbcc4a121d37998
Eric Wiseblatt <ewiseblatt@google.com>
2016-06-11 08:37:47 -0400

    Delete image created as side effect to the bake and deploy test.
    Adds mechanism to cleanup from tests.
    Some lint fixes.

--- citest/service_testing/operation_contract.py
+++ citest/service_testing/operation_contract.py
@@ -38,17 +38,42 @@ class OperationContract(JsonSnapshotable):
     """The json.Contract to verify the operation."""
     return self.__contract
 
+  @property
+  def status_collector(self):
+    """The callable(OperationStatus) if any was bound.
+
+    The StatusCollector can be used to observe the operation status
+    and collect any information from it.
+    """
+    return self.__status_collector
+
+  @property
+  def cleanup(self):
+    """The callable(OperationStatus, ContractVerifyResult) if any was bound.
+
+    The cleanup can be used to perform any post-operation cleanup
+    after the contract has been verified. It is called regardless of
+    success or failure.
+    """
+    return self.__cleanup
+
   def export_to_json_snapshot(self, snapshot, entity):
     """Implements JsonSnapshotable interface."""
     snapshot.edge_builder.make(entity, 'Operation', self.__operation)
     snapshot.edge_builder.make(entity, 'Contract', self.__contract)
 
-  def __init__(self, operation, contract):
+  def __init__(self, operation, contract,
+               status_collector=None, cleanup=None):
     """Construct instance.
 
     Args:
       operation: [AgentOperation] To be performed.
       contract: [JsonContract] To verify operation.
+      status_collector: [Callable(OperationStatus)] Takes OperationStatus.
+      cleanup: [Callable(OperationStatus, ContractVerifyResult)]
+          Perform any post-test cleanup.
     """
     self.__operation = operation
     self.__contract = contract
+    self.__status_collector = status_collector
+    self.__cleanup = cleanup
